6c5aa9f79ab74589f1c3d935110dd2f18dfcd64b
tufusa <tufusa74@gmail.com>
2024-09-01 11:05:00 +0900

    feat: リクエスト検証用ミドルウェア

---
+++ src/middleware/verifyKeyMiddleware.ts
@@ -0,0 +1,23 @@
+import { verifyKey } from "discord-interactions";
+import { Bindings } from "..";
+import { createMiddleware } from "hono/factory";
+
+export const verifyKeyMiddleware = createMiddleware<{ Bindings: Bindings }>(
+  async (c, next) => {
+    const signature = c.req.header("X-Signature-Ed25519");
+    const timestamp = c.req.header("X-Signature-Timestamp");
+    const body = await c.req.text();
+
+    const isValid =
+      signature != null &&
+      timestamp != null &&
+      (await verifyKey(body, signature, timestamp, c.env.BOT_PUBLIC_KEY));
+
+    if (!isValid) {
+      console.warn("Invalid request signature");
+      return c.text("Bad request signature", 401);
+    }
+
+    return next();
+  }
+);
