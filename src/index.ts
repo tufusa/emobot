3a112682acfadcd61290ff5d59897db6ad1aa909
tufusa <tufusa74@gmail.com>
2024-09-01 11:04:35 +0900

    feat: バインディングの定義

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { Hono } from "hono";
 
+export type Bindings = {
+  BOT_TOKEN: string;
+  BOT_PUBLIC_KEY: string;
+  BOT_APPLICATION_ID: string;
+};
+
 const app = new Hono();
 
 app.get("/", (c) => {
